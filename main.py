f90568f90f5e443d897c85bd0a50a846fc6e082a
Zac Chan <sunshinecltzac@gmail.com>
2018-11-05 00:01:35 -0500

    main.py update

--- main.py
+++ main.py
@@ -19,5 +19,6 @@ def neighbor(params):
 rhc = RandomHillClimbing(params, neighbor, nn, max_try_per_step=10000)
 rhc.find()
 
-print("Validation acc: %d" % nn.evaluate_on_dataset(rhc.params, dataset="val"))
-print("Test acc: %d" % nn.evaluate_on_dataset(rhc.params, dataset="test"))
+print("Training acc: %f" % nn.evaluate_on_dataset(rhc.params, dataset="train") / nn.y_train.shape[0])
+print("Validation acc: %f" % nn.evaluate_on_dataset(rhc.params, dataset="val") / nn.y_val.shape[0])
+print("Test acc: %f" % nn.evaluate_on_dataset(rhc.params, dataset="test") / nn.y_test.shape[0])
